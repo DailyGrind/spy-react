41431f24f2e26db12c16953d44b9965bba8799e4
fitzk <kayla.fitzsimmons@protonmail.com>
2017-02-24 01:45:38 -0800

    clean up event handler factory

--- dev/app.js
+++ dev/app.js
@@ -2,24 +2,28 @@ import React, { Component } from "react";
 import spy from "./../lib";
 import { Button } from "./button";
 
-@spy
 export class App extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
 			event: 0
 		}
+		this.handleClick = () => {
+			this.setState({
+				event: this.state.event + 1
+			});
+		}
 	}
-	onClick() {
-		this.setState({
-			event: this.state.event + 1
-		});
+
+	shouldComponentUpdate(){
+		return true;
 	}
+
 	render() {
 		return <div>
-						<Button count={ this.state.event }  onClick={ this.onClick.bind(this) }>
+						<Button count={ this.state.event }  onClick={ this.handleClick }>
 							<label>Click Me!</label>
 						</Button>
-					</div>
+					 </div>
 	}
 }
